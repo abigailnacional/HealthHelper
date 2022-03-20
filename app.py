1b62858b6ab4e9fdc1cc1d9d1d284f71cc07654d
Abigail Nacional <aan16@duke.edu>
2022-03-19 23:08:47 -0400

    Figured out how to make API requests

--- app.py
+++ app.py
@@ -3,6 +3,9 @@ from appmodels import create_app
 import os
 from flask_sqlalchemy import SQLAlchemy
 import sqlalchemy.orm
+import requests
+from requests.structures import CaseInsensitiveDict
+import json
 
 app = create_app()
 db = SQLAlchemy(app)
@@ -10,7 +13,25 @@ sessionmaker = sqlalchemy.orm.sessionmaker(db.engine)
 
 @app.route("/")
 def index():
+     #request()
      return render_template("index.html")
+
+#Make form for text intake
+
+def request():
+     url = "https://healthcare.googleapis.com/v1/projects/healthhelper-344619/locations/us-central1/services/nlp:analyzeEntities"
+     headers = CaseInsensitiveDict()
+     headers["Authorization"] = "Bearer ya29.c.b0AXv0zTO_To8DxLoDxnm56FPPLMmwnbYRLSShoxRvFe3pa_AUJcyQl7tbLb_H-Bvjt-lP9sUegdR2XtZJcSEOMPLLq3NmDF3mGBFE2OPBBvhUDCgCqajuhEWhhi_W_KA2VITxD1skoEhBC_ddJKr-RkZ-Td-2RUWo-RuxiETkvIqJmC4qXkKdBZghUoaiVZErtOU8bCKGWwWiYsG9-bFbx7bGBvZvuvg"
+     headers["Content-Type"] = "application/json"
+     data = '{"nlpService":"projects/healthhelper-344619/locations/us-central1/services/nlp","documentContent":"Insulin regimen human 5 units IV administered.","licensedVocabularies":["SNOMEDCT_US","ICD10CM"]}'
+     resp = requests.post(url, headers=headers, data=data)
+     print(resp.status_code)
+     if resp.status_code == 200:
+          data = json.loads(resp.text)
+          print(data['entityMentions'])
+          #Get every mention
+          print(len(data['entityMentions']))
+     return None
 
 if __name__ == "__main__":
   app.run()
